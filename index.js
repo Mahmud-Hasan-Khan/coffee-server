a2f87b79a3e3c462f0086a9edbbf220a90e94152
Mahmud Hasan <mahmud.hasan@ymail.com>
2023-10-15 17:09:07 +0600

    vercel deployment done

--- index.js
+++ index.js
@@ -28,6 +28,8 @@ async function run() {
 
         const coffeeCollection = client.db('coffeeDB').collection('coffee');
 
+        // user database
+        const userCollection = client.db('coffeeDB').collection('user')
         // step 2
         // get api data from server
         app.get('/coffee', async (req, res) => {
@@ -85,6 +87,39 @@ async function run() {
 
         })
 
+        // user related api
+
+        app.post('/users', async (req, res) => {
+            const newUser = req.body;
+            console.log(newUser);
+            const result = await userCollection.insertOne(newUser);
+            res.send(result);
+        })
+
+        app.get('/users', async (req, res) => {
+            const cursor = userCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await userCollection.deleteOne(query);
+            res.send(result);
+        })
+
+        app.patch('/users', async (req, res) => {
+            const updateUser = req.body;
+            const filter = { email: updateUser.email };
+            const updateDoc = {
+                $set: {
+                    lastLoggedAt: updateUser.lastLoggedAt
+                }
+            }
+            const result = await userCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        })
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
